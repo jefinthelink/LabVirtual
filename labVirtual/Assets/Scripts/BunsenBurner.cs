35a61a75f18b6c00639485df6c0fa14971460a30
jefinthelink <jefinzabotto@gmail.com>
2022-01-04 12:32:22 -0300

    animação do bico de bulsen

--- labVirtual/Assets/Scripts/BunsenBurner.cs
+++ labVirtual/Assets/Scripts/BunsenBurner.cs
@@ -7,21 +7,32 @@ public class BunsenBurner : MonoBehaviour
 
     [SerializeField] private GameObject fireCollider;
     [SerializeField] private ParticleSystem psFire, psFire2;
+    [SerializeField] private Animator collarAnimator;
     private bool turnOnFire = false;
+    private Vector3 initialRotationCollar, finalRotateCollar;
+    private void Start()
+    {
+
+        
+    }
     public void HandleTurnOnFire()
     {
         if (turnOnFire)
         {
+            Debug.Log("acendendo");
             turnOnFire = false;
-
+            collarAnimator.SetBool("TurnOn", false);
+            collarAnimator.SetBool("TurnOf", true);
             fireCollider.GetComponent<FireCollider>().DoNotBurnCube();
             psFire.Stop();
             psFire2.Stop();
         }
         else
         {
+            Debug.Log("apagando");
             turnOnFire = true;
-            
+            collarAnimator.SetBool("TurnOn", true);
+            collarAnimator.SetBool("TurnOf", false);
             fireCollider.GetComponent<FireCollider>().BurnCube();
              psFire.Play();
             psFire2.Play();
